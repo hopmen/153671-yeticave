4ae95000c983263067f0cecade452d380a715b8d
Hopman <3tonchik@mail.ru>
2018-11-24 17:00:40 +0300

    module3-task1

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Шаблонизатор
+ *
+ * @param string $name      название поключаемого файла
+ * @param array $data       массив с данными для поключаемого файла
+ * @return false|string     верстка шаблона
+ */
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    /** @var false|string $result результат выполнения вукции */
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+/**
+ * получить форматированую цену с знаком рубля
+ *
+ * @param integer $number       число для преобразования
+ * @return string               форматирваная цена с знаком рубля(b.rub)
+ */
+function get_format_price($number){
+    $number = ceil($number);
+    return number_format($number,0,null, ' ') . ' <b class="rub">р</b>';
+}
