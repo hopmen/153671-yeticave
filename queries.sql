2329b0639575dfd2e2e8cd16962df430ed62f810
Hopman <3tonchik@mail.ru>
2018-12-11 22:06:44 +0300

    module4-task2

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+USE `153671-yeticave`;
+
 INSERT INTO `categories` (`name`)
 	VALUES ('Доски и лыжи'),
 	       ('Крепления'),
@@ -20,45 +22,39 @@ INSERT INTO `lots`
 	VALUES
 	  ('2014 Rossignol District Snowboard', 								'2018-12-31 23:59:59','img/lot-1.jpg', 	10999, 					100, 					1, 						1),
  		('DC Ply Mens 2016/2017 Snowboard', 									'2018-12-31 23:59:59','img/lot-2.jpg', 	159999, 				100, 					2, 						2),
-    ('Крепления Union Contact Pro 2015 года размер L/XL', '2018-12-31 23:59:59','img/lot-3.jpg', 	8000, 					100, 					3, 						3),
+    ('Крепления Union Contact Pro 2015 года размер L/XL', '2018-12-31 23:59:59','img/lot-3.jpg', 	8000, 					100, 					1, 						3),
     ('Ботинки для сноуборда DC Mutiny Charocal', 					'2018-12-31 23:59:59','img/lot-4.jpg', 	10999, 					100, 					1, 						4),
     ('Куртка для сноуборда DC Mutiny Charocal', 					'2018-12-31 23:59:59','img/lot-5.jpg', 	7500, 					100, 					2, 						5),
-    ('Маска Oakley Canopy', 															'2018-12-31 23:59:59','img/lot-6.jpg', 	5400, 					100, 					3, 						6);
+    ('Маска Oakley Canopy', 															'2018-12-31 23:59:59','img/lot-6.jpg', 	5400, 					100, 					2, 						6);
 
 
 INSERT INTO `bets`
   	(`price`, 	`author_id`,	`lot_id`)
 	VALUES
-		(11299, 		1, 						1),
-		(8100, 			2, 						2);
+		(11299, 		2, 						1),
+		(170000,		1, 						2),
+    (12299, 		2, 						1),
+    (180000, 		1, 						2);
 
 -------------------------------------------------------
 
 
 -- Получить все категории
-SELECT `name` FROM `categories`;
+SELECT * FROM `categories`;
 
 
 -- Получить самые новые, открытые лоты.
-SELECT l.title, l.price, l.img_path, c.name
+SELECT l.title, l.price start_price, l.img_path, c.name, MAX(b.price) price
 	FROM lots l
-		JOIN categories c ON c.id = l.category_id
-			WHERE l.end_time != current_timestamp()
-				ORDER BY l.start_time DESC;
+		LEFT JOIN categories c ON c.id = l.category_id
+    	LEFT JOIN bets b ON b.lot_id = l.id
+				WHERE l.end_time >= now() AND l.author_id IS NOT NULL
+					GROUP BY l.id
+						ORDER BY l.start_time DESC;
 
 
 -- Показать лот по его id.
-SELECT l.`id`,
-       l.`title`,
-       l.`category_id`,
-       l.`description`,
-       l.`img_path`,
-       l.`price`,
-       l.`bets_step`,
-       l.`start_time`,
-       l.`end_time`,
-       l.`author_id`,
-       l.`winner_id`,
+SELECT l.*,
        c.`name`
 	FROM `lots` l
 		INNER JOIN `categories` c ON l.`category_id` = c.`id`
@@ -72,7 +68,7 @@ UPDATE `lots` l
 
 
 -- Получить список самых свежих ставок для лота по его id.
-SELECT b.`date`, b.`price`, b.`author_id`
+SELECT b.*
 	FROM `bets`b
-		WHERE b.`lot_id` = 3
+		WHERE b.`lot_id` = 2
 			ORDER BY b.`date` DESC;
